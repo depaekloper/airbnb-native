e79101d0973b1cc8eb0eb84221affb58c5fe074b
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-10-03 11:12:36 +0900

    #6.2 Preloading Images

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, Image } from "react-native";
+
+const cacheImages = images =>
+  images.map(image => {
+    console.log(image);
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = fonts => fonts.map(font => Font.loadAsync(font));
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => setIsReady(true);
+  const loadAssets = async () => {
+    const images = [
+      require("./assets/loginBg.jpg"),
+      "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png",
+    ];
+    const fonts = [Ionicons.font];
+    const imagePromises = cacheImages(images);
+    const fontPromises = cacheFonts(fonts);
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  return isReady ? (
+    <Text>I'm ready</Text>
+  ) : (
+    <AppLoading
+      onError={console.error}
+      onFinish={handleFinish}
+      startAsync={loadAssets}
+    />
+  );
+}

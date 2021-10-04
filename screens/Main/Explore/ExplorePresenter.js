f3253d47e20005786db4146a29f0f7c37fbcab03
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-10-04 20:55:24 +0900

    #9.6 RoomCard part Two

--- screens/Main/Explore/ExplorePresenter.js
+++ screens/Main/Explore/ExplorePresenter.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components/native";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, ScrollView } from "react-native";
 import RoomCard from "../../../components/RoomCard";
+
 // import RoomCard from "../../../components/RoomCard";
 
 const Container = styled.View`
@@ -10,25 +11,28 @@ const Container = styled.View`
   align-items: center;
 `;
 
-const Text = styled.Text``;
-
 export default ({ rooms }) => {
   return (
     <Container>
       {rooms.length === 0 ? (
         <ActivityIndicator color="black" />
       ) : (
-        rooms.map(room => (
-          <RoomCard
-            key={room.id}
-            name={room.name}
-            price={room.price}
-            photos={room.photos}
-            id={room.id}
-            isFav={room.is_fav}
-            isSuperHost={room.user.superhost}
-          />
-        ))
+        <ScrollView
+          style={{ width: "100%", marginTop: 120 }}
+          contentContainerStyle={{ paddingHorizontal: 15 }}
+        >
+          {rooms.map(room => (
+            <RoomCard
+              key={room.id}
+              name={room.name}
+              price={room.price}
+              photos={room.photos}
+              id={room.id}
+              isFav={room.is_fav}
+              isSuperHost={room.user.superhost}
+            />
+          ))}
+        </ScrollView>
       )}
     </Container>
   );

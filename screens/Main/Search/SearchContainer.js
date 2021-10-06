7fba750444b6d88b08a4cb6a516d02ef5be5ddd7
Paek Seungwoo <paekseungwoo@Paekui-Macmini.local>
2021-10-06 23:25:27 +0900

    #12.5 Recap and Token

--- screens/Main/Search/SearchContainer.js
+++ screens/Main/Search/SearchContainer.js
@@ -1,4 +1,51 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/core";
+import React, { useState } from "react";
+import { Keyboard } from "react-native";
 import SearchPresenter from "./SearchPresenter";
+import api from "../../../api";
 
-export default () => <SearchPresenter />;
+export default token => {
+  const navigation = useNavigation();
+  const [searching, setSarching] = useState(false);
+  const [beds, setBeds] = useState();
+  const [bedrooms, setBedrooms] = useState();
+  const [bathrooms, setBathrooms] = useState();
+  const [maxPrice, setMaxPrice] = useState();
+  const [results, setResults] = useState();
+  const triggerSearch = async () => {
+    // call the api
+    setSarching(true);
+    const form = {
+      ...(beds && { beds }),
+      ...(bedrooms && { bedrooms }),
+      ...(bathrooms && { bathrooms }),
+      ...(maxPrice && { max_price: maxPrice }),
+    };
+    try {
+      const { data } = await api.search(form, token);
+      setResults(data);
+      console.log(data);
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      Keyboard.dismiss();
+      setSarching(false);
+    }
+  };
+  return (
+    <SearchPresenter
+      navigation={navigation}
+      beds={beds}
+      setBeds={setBeds}
+      bedrooms={bedrooms}
+      setBedrooms={setBedrooms}
+      bathrooms={bathrooms}
+      setBathrooms={setBathrooms}
+      maxPrice={maxPrice}
+      setMaxPrice={setMaxPrice}
+      searching={searching}
+      triggerSearch={triggerSearch}
+      results={results}
+    />
+  );
+};
